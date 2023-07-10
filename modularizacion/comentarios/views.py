2c5dea628fb5a928f4a1394fb557b4266e432abd
Mari Carmen García Cortés <garciacortesmariadelcarmen@gmail.com>
2023-07-10 11:48:47 +0200

    3.4 Creation and deletion of data

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Comment
 
 def test (request):
     return HttpResponse('Funciona correctamente')
 
 def create (request):
+    #comment = Comment(name='Manuela', score=5, comment='Este es un comentario')
+    #comment.save()
+    #comment = Comment.objects.create(name='Mina')
     return HttpResponse('Ruta para probar la creación de modelos')
+
+def delete (request):
+    comment = Comment.objects.get(id=5)
+    comment.delete()   
+    Comment.objects.filter(id=2).delete()
+
+    return HttpResponse('Ruta para probar los borrados')
