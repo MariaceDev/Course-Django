1f35e944271ecd11cf677c4acc1049ee10a6ae55
Mari Carmen García Cortés <garciacortesmariadelcarmen@gmail.com>
2023-07-28 11:05:57 +0200

    3.8. Data Query II

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -16,5 +16,15 @@ def queries (request):
 
     # Obtener 10 elemetos saltando los 5 primeros
     offsets = Author.objects.all()[5:10] 
+
+    # Obtener todos los elementos ordenados
+    orders = Author.objects.all().order_by('email')
+
+    # Obtener todos los elementos cuyo id sea menor o igual a 15
+    filtereds2 = Author.objects.filter(id__lte=15)
+
+    #Obtener todos los autores que contienen en su nombre la palabra 'yes'
+
+    contains = Author.objects.filter(name__contains='yes')
   
-    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets':  offsets})
+    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author': author, 'limits': limits, 'offsets':  offsets, 'orders': orders, 'filtereds2': filtereds2, 'contains': contains})
